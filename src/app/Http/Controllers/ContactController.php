40fcc6ad245890abfdb06c1a5bd9c0ffd3ca9b58
yui kondo <yui.jump.0406@gmail.com>
2024-11-17 19:43:06 +0900

    コメント

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+  public function index()
+  {
+    return view('index');
+  }
+
+  public function confirm(Request $request)
+     {
+         $contact = $request->only(['name', 'email', 'tel', 'content']);
+         return view('confirm', compact('contact'));
+     }
+  public function store(Request $request)
+     {
+         $contact = $request->only(['name', 'email', 'tel', 'content']);
+         Contact::create($contact);
+         
+         $contact->timestamps = false;
+         
+         return view('thanks');
+}
+}
+
